480d8d4084faa6e0996ad9eb0f3a76dedd4e27a2
GalenByrd <galen.byrd@gmail.com>
2018-09-04 10:56:26 -0400

    Toast

--- Desktop/CS 275/Simplest/app/src/main/java/com/example/galenbyrd/simplest/MainActivity.java
+++ Desktop/CS 275/Simplest/app/src/main/java/com/example/galenbyrd/simplest/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.galenbyrd.simplest;
 
+
+
 import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "Simplest";
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mButton = (Button) findViewById(R.id.my_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick");
+                Toast.makeText(v.getContext(), "click", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
